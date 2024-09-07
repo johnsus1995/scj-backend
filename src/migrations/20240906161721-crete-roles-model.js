c4d3aca3297dfed07d0855e00c2e6ba174fcb1f2
johnsus1995 <jaisonjohny26@gmail.com>
2024-09-07 10:52:39 +0530

    exam model

--- src/migrations/20240906161721-crete-roles-model.js
+++ src/migrations/20240906161721-crete-roles-model.js
@@ -12,12 +12,12 @@ module.exports = {
       createdAt: {
         type: Sequelize.DATE,
         allowNull: false,
-        defaultValue: Sequelize.fn('NOW'),
+        defaultValue: Sequelize.fn("NOW"),
       },
       updatedAt: {
         type: Sequelize.DATE,
         allowNull: false,
-        defaultValue: Sequelize.fn('NOW'),
+        defaultValue: Sequelize.fn("NOW"),
       },
       deletedAt: {
         type: Sequelize.DATE,
--- src/models/role.js
+++ src/models/role.js
@@ -1,13 +1,17 @@
 module.exports = (sequelize, DataTypes) => {
-  const Role = sequelize.define("Role", {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  const Role = sequelize.define(
+    "Role",
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
     },
-  },{
-    timestamps: true,
-    paranoid: true,
-  });
+    {
+      timestamps: true,
+      paranoid: true,
+    }
+  );
 
   // eslint-disable-next-line func-names
   Role.associate = function (models) {
